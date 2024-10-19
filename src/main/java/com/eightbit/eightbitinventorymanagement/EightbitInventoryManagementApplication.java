a7d6588c0d0271fc506b6d981aee9a7f61aa07ad
Jiwen <given209@terpmail.umd.edu>
2024-10-18 19:08:59 -0700

    Initial project

---
+++ src/main/java/com/eightbit/eightbitinventorymanagement/EightbitInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.eightbit.eightbitinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightbitInventoryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EightbitInventoryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eightbit/eightbitinventorymanagement/EightbitInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eightbit.eightbitinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EightbitInventoryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
